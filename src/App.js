6a0bed55c1efa3a6304fdbb59a8715d89b4e636d
jackiewu99 <Jackie.Wu99@myhunter.cuny.edu>
2021-11-14 20:54:08 -0500

    completedBaseCode

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './components/Home';
+import UserProfile from './components/UserProfile';
+//import Debit from './components/Debit'
+//import Credit from './components/Credit'
+import LogIn from './components/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//import axios from "axios";
+
+class App extends Component {
+
+  mockLogIn = (logInInfo) => {
+    const newUser = {...this.state.currentUser}
+    newUser.userName = logInInfo.userName
+    this.setState({currentUser: newUser})
+  }
+
+  constructor() {
+    
+    super();
+    this.state = {
+      
+      accountBalance: 0,
+      currentUser: {
+        userName: 'joe_shmo',
+        memberSince: '07/23/96',
+      },
+     // debits: [],
+     // credits: []
+    }  
+  }
+
+  
+  
+
+
+
+  render() {
+    const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
+    const UserProfileComponent = () => (<UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince}  /> );
+    const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />);
+    return (
+        <Router>
+          <div>
+
+            <Route exact path="/" render={HomeComponent}/>
+            <Route exact path="/userProfile" render={UserProfileComponent}/>
+            <Route exact path="/login" render={LogInComponent}/>
+        
+          </div>
+        </Router>
+    );
+  }
+
 }
 
-export default App;
-//test change
+export default App
